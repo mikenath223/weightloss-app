c4d979626a053db57035630bf5801bc3a5ef4936
Michgolden Ukeje <michgoldennathan17@gmail.com>
2024-11-16 10:29:51 +0100

    Integrate skeleton ui and tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,24 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
-  theme: {
-    extend: {}
-  },
-
-  plugins: [typography, forms]
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		typography,
+		forms,
+		skeleton({
+			themes: { preset: ['seafoam'] }
+		})
+	]
 } satisfies Config;
