43f7ea04c319f63ff57066285a07b5aaec17d33f
Michgolden Ukeje <michgoldennathan17@gmail.com>
2024-11-20 08:16:26 +0100

    Update styling on page header

--- src/lib/components/ui/PageHeaderView.svelte
+++ src/lib/components/ui/PageHeaderView.svelte
@@ -2,7 +2,7 @@
 	let { title, subtitle } = $props();
 </script>
 
-<header class="mb-28 text-center">
+<header class="mb-16 text-center md:mb-28">
 	<h1 class="mb-10 text-2xl font-extrabold text-indigo-700 drop-shadow-md md:text-5xl">
 		{title}
 	</h1>
